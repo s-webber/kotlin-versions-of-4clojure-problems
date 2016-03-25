2050538515b38e659dad2b252f7055ed64dc01e0
s-webber <webbers.github@gmail.com>
2016-03-25 19:54:07 +0000

    power set

---
+++ src/main/kotlin/kotlin4clojure/medium/power_set/PowerSet.kt
@@ -0,0 +1,6 @@
+package kotlin4clojure.medium.power_set
+
+fun <T> powerSet(input: Set<T>): Set<Set<T>> {
+    // TODO Add your solution here.
+    throw RuntimeException()
+}
---
+++ src/test/kotlin/kotlin4clojure/medium/power_set/PowerSetTest.kt
@@ -0,0 +1,37 @@
+package kotlin4clojure.medium.power_set
+
+import kotlin.test.assertEquals
+import org.junit.Test
+
+/**
+ * 85. Power Set
+ * 
+ * Write a function which generates the power set of a given set.
+ * The power set of a set x is the set of all subsets of x, including the empty set and x itself.
+ */
+class PowerSetTest {
+    @Test fun f1() {
+        val expected = setOf(setOf(1, "a"), setOf("a"), emptySet(), setOf(1))
+        val actual = powerSet(setOf(1, "a"))
+        assertEquals(expected, actual)
+    }
+
+    @Test fun f2() {
+        val expected = setOf(emptySet<Any>())
+        val actual = powerSet(emptySet<Any>())
+        assertEquals(expected, actual)
+    }
+
+    @Test fun f3() {
+        val expected = setOf(emptySet(),
+                             setOf(1), setOf(2), setOf(3),
+                             setOf(1, 2), setOf(1, 3), setOf(2, 3),
+                             setOf(1, 2, 3))
+        val actual = powerSet(setOf(1, 2, 3))
+        assertEquals(expected, actual)
+    }
+
+    @Test fun f4() {
+        assertEquals(1024, powerSet((0..9).toHashSet()).size)
+    }
+}
