f91f291c281394c04ec3c894ccc7a2e2ff12d235
s-webber <webbers.github@gmail.com>
2016-03-25 17:05:19 +0000

    filter perfect squares

---
+++ src/main/kotlin/kotlin4clojure/medium/filter_perfect_squares/FilterPerfectSquares.kt
@@ -0,0 +1,6 @@
+package kotlin4clojure.medium.filter_perfect_squares
+
+fun filterPerfectSquares(input:String): String {
+    // TODO Add your solution here.
+    throw RuntimeException()
+}
---
+++ src/test/kotlin/kotlin4clojure/medium/filter_perfect_squares/FilterPerfectSquaresTest.kt
@@ -0,0 +1,20 @@
+package kotlin4clojure.medium.filter_perfect_squares
+
+import kotlin.test.assertEquals
+import org.junit.Test
+
+/**
+ * 74. Filter Perfect Squares
+ * 
+ * Given a string of comma separated integers,
+ * write a function which returns a new comma separated string that only contains the numbers which are perfect squares.
+ */
+class FilterPerfectSquaresTest {
+    @Test fun f1() {
+        assertEquals("4,9", filterPerfectSquares("4,5,6,7,8,9"))
+    }
+
+    @Test fun f2() {
+        assertEquals("16,25,26", filterPerfectSquares("15,16,25,36,37"))
+    }
+}
