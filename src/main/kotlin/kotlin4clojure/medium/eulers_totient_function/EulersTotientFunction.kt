27f7af6d5a788eb5370106d37ad5b0ef6ab85a07
s-webber <webbers.github@gmail.com>
2016-03-25 16:55:17 +0000

    Euler's totient function

---
+++ src/main/kotlin/kotlin4clojure/medium/eulers_totient_function/EulersTotientFunction.kt
@@ -0,0 +1,6 @@
+package kotlin4clojure.medium.eulers_totient_function
+
+fun eulerTotient(input: Int): Int {
+    // TODO Add your solution here.
+    throw RuntimeException()
+}
---
+++ src/test/kotlin/kotlin4clojure/medium/eulers_totient_function/EulersTotientFunctionTest.kt
@@ -0,0 +1,29 @@
+package kotlin4clojure.medium.eulers_totient_function
+
+import kotlin.test.assertEquals
+import org.junit.Test
+
+/**
+ * 75. Euler's Totient Function
+ * 
+ * Two numbers are coprime if their greatest common divisor equals 1.
+ * Euler's totient function f(x) is defined as the number of positive integers less than x which are coprime to x.
+ * The special case f(1) equals 1. Write a function which calculates Euler's totient function.
+ */
+class EulersTotientFunctionTest {
+    @Test fun f1() {
+        assertEquals(1, eulerTotient(1))
+    }
+
+    @Test fun f2() {
+        assertEquals(4, listOf(1, 3, 7, 9).size)
+    }
+
+    @Test fun f3() {
+        assertEquals(16, eulerTotient(40))
+    }
+
+    @Test fun f4() {
+        assertEquals(60, eulerTotient(99))
+    }
+}
