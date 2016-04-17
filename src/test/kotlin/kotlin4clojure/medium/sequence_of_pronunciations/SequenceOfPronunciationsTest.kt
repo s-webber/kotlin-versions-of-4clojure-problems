240f49bfae5a648b0573855c923a3018270ad97b
s-webber <webbers.github@gmail.com>
2016-04-17 11:07:38 +0100

    sequence of pronunciations

--- src/test/kotlin/kotlin4clojure/medium/sequence_of_pronunciations/SequenceOfPronunciationsTest.kt
+++ src/test/kotlin/kotlin4clojure/medium/sequence_of_pronunciations/SequenceOfPronunciationsTest.kt
@@ -6,8 +6,8 @@ import org.junit.Test
 /**
  * 110. Sequence of pronunciations
  * 
- * Write a function that returns a lazy sequence of "pronunciations" of a sequence of numbers. A pronunciation of each
- * element in the sequence consists of the number of repeating identical numbers and the number itself. For example,
+ * Write a function that returns a lazy sequence of "pronunciations" of a list of numbers. A pronunciation of each
+ * element in the list consists of the number of repeating identical numbers and the number itself. For example,
  * [1 1] is pronounced as [2 1] ("two ones"), which in turn is pronounced as [1 2 1 1] ("one two, one one").
  *
  * Your function should accept an initial sequence of numbers, and return an infinite lazy sequence of pronunciations,
