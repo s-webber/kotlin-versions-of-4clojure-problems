b132fe1e82a151886f17ec201d778960190147d5
s-webber <webbers.github@gmail.com>
2016-04-24 09:19:19 +0100

    re-implement iterate

---
+++ src/test/kotlin/kotlin4clojure/easy/reimplement_iterate/ReimplementIterateTest.kt
@@ -0,0 +1,30 @@
+package kotlin4clojure.easy.reimplement_iterate
+
+import kotlin.test.assertEquals
+import org.junit.Test
+
+/**
+ * 62. Re-implement Iterate
+ * 
+ * Given a side-effect free function f and an initial value x write a function
+ * which returns an infinite lazy sequence of x, (f x), (f (f x)), (f (f (f x))), etc.
+ */
+class ReimplementIterateTest {
+    @Test fun f1() {
+        val expected = sequenceOf(1, 2, 4, 8, 16)
+        val actual = reimplementIterate({it * 2}, 1).take(5)
+        assertEquals(expected, actual)
+    }
+
+    @Test fun f2() {
+        val expected = (0..99).asSequence()
+        val actual = reimplementIterate({it + 1}, 0).take(100)
+        assertEquals(expected, actual)
+    }
+
+    @Test fun f3() {
+        val expected = sequenceOf(1, 2, 3, 1, 2, 3, 1, 2, 3)
+        val actual = reimplementIterate({(it % 3) + 1}, 1).take(9)
+        assertEquals(expected, actual)
+    }
+}
