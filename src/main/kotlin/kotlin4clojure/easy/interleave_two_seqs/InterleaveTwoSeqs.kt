11df57d63361872ee3f265dae718c0fbe2ecd904
s-webber <webbers.github@gmail.com>
2016-04-24 09:13:09 +0100

    interleave two seqs

---
+++ src/main/kotlin/kotlin4clojure/easy/interleave_two_seqs/InterleaveTwoSeqs.kt
@@ -0,0 +1,5 @@
+package kotlin4clojure.easy.interleave_two_seqs
+
+fun <T> interleaveTwoSequences(a: Sequence<T>, b: Sequence<T>): Sequence<T> {
+    TODO("Add your solution here")
+}
---
+++ src/test/kotlin/kotlin4clojure/easy/interleave_two_seqs/InterleaveTwoSeqsTest.kt
@@ -0,0 +1,36 @@
+package kotlin4clojure.easy.interleave_two_seqs
+
+import kotlin.test.assertEquals
+import org.junit.Test
+
+/**
+ * 39. Interleave Two Seqs
+ * 
+ * Write a function which takes two sequences and returns the first item from each,
+ * then the second item from each, then the third, etc.
+ */
+class InterleaveTwoSeqsTest {
+    @Test fun f1() {
+        val expected = sequenceOf<Any>(1, 'a', 2, 'b', 3, 'c')
+        val actual = interleaveTwoSequences(sequenceOf(1, 2, 3), sequenceOf('a', 'b', 'c'))
+        assertEquals(expected, actual)
+    }
+
+    @Test fun f2() {
+        val expected = sequenceOf(1, 3 ,2, 4)
+        val actual = interleaveTwoSequences(sequenceOf(1, 2), sequenceOf(3, 4, 5, 6))
+        assertEquals(expected, actual)
+    }
+
+    @Test fun f3() {
+        val expected = sequenceOf(1, 5)
+        val actual = interleaveTwoSequences(sequenceOf(1, 2, 3, 4), sequenceOf(5))
+        assertEquals(expected, actual)
+    }
+
+    @Test fun f4() {
+        val expected = sequenceOf(30, 25, 20, 15)
+        val actual = interleaveTwoSequences(sequenceOf(30, 20), sequenceOf(25, 15))
+        assertEquals(expected, actual)
+    }
+}
