9e563ebd0dd01910484e4bc088265331c1e5e80a
s-webber <webbers.github@gmail.com>
2016-04-30 14:25:08 +0100

    beauty is symmetry solution

--- src/main/kotlin/kotlin4clojure/easy/beauty_is_symmetry/BeautyIsSymmetry.kt
+++ src/main/kotlin/kotlin4clojure/easy/beauty_is_symmetry/BeautyIsSymmetry.kt
@@ -1,7 +1,13 @@
 package kotlin4clojure.easy.beauty_is_symmetry
 
-fun <T> beautyIsSymmetry(input: Node<T>): Boolean {
-    TODO("Add your solution here")
+fun <T> beautyIsSymmetry(n: Node<T>) = n.left == reverseTree(n.right)
+
+fun <T> reverseTree(n: Node<T>?): Node<T>? {
+    if (n == null) {
+        return null;
+    } else {
+        return Node(n.id, reverseTree(n.right), reverseTree(n.left))
+    }
 }
 
 data class Node<T> (val id: T, val left: Node<T>? = null, val right: Node<T>? = null)
