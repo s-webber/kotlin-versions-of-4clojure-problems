068df48005a8ac28980e5b4279dba9b61398b322
s-webber <webbers.github@gmail.com>
2016-04-30 11:31:59 +0100

    reverse list solution

--- src/main/kotlin/kotlin4clojure/easy/reverse_a_list/ReverseList.kt
+++ src/main/kotlin/kotlin4clojure/easy/reverse_a_list/ReverseList.kt
@@ -1,5 +1,3 @@
 package kotlin4clojure.easy.reverse_a_list
 
-fun <T> reverseList(input: List<T>): List<T> {
-    TODO("Add your solution here")
-}
+fun <T> reverseList(input: List<T>) = input.asReversed()
