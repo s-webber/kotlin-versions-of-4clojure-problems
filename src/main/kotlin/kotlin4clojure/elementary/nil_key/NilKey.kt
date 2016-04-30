fbfe0f103f2b32184b14b3407615fced6b640683
s-webber <webbers.github@gmail.com>
2016-04-30 11:24:33 +0100

    nil key solution

--- src/main/kotlin/kotlin4clojure/elementary/nil_key/NilKey.kt
+++ src/main/kotlin/kotlin4clojure/elementary/nil_key/NilKey.kt
@@ -1,5 +1,3 @@
 package kotlin4clojure.elementary.nil_key
 
-fun <K, V> nilKey(key: K, map: Map<K, V>): Boolean {
-    TODO("Add your solution here")
-}
+fun <K, V> nilKey(key: K, map: Map<K, V>) = map.containsKey(key) && map[key] == null
