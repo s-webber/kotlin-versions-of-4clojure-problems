589c2655135daa1237aff04978ac73045bee431b
s-webber <webbers.github@gmail.com>
2016-04-30 13:45:46 +0100

    map construction solution

--- src/main/kotlin/kotlin4clojure/easy/map_construction/MapConstruction.kt
+++ src/main/kotlin/kotlin4clojure/easy/map_construction/MapConstruction.kt
@@ -1,5 +1,5 @@
 package kotlin4clojure.easy.map_construction
 
 fun <K, V> mapConstruction(keys: List<K>, values: List<V>): Map<K, V> {
-    TODO("Add your solution here")
+    return keys.mapIndexed { i, k -> Pair(k, values[i]) }.toMap()
 }
