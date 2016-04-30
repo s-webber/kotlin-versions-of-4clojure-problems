982326f1ea2bbf61bfbfa8cea4fd1f20532ad675
s-webber <webbers.github@gmail.com>
2016-04-30 16:12:24 +0100

    tricky card games solution

--- src/main/kotlin/kotlin4clojure/medium/tricky_card_games/TrickyCardGames.kt
+++ src/main/kotlin/kotlin4clojure/medium/tricky_card_games/TrickyCardGames.kt
@@ -1,11 +1,17 @@
 package kotlin4clojure.medium.tricky_card_games
 
 fun trickyCardGames(trump: Suit?): (List<Card>) -> Card {
-    TODO("Add your solution here")
+    return { input ->
+        input.reduce { a, b ->
+            when {
+                b.suit == trump && a.suit != trump -> b
+                b.suit == a.suit && b.rank > a.rank -> b
+                else -> a
+            }
+        }
+    }
 }
 
 data class Card(val suit: Suit, val rank: Int)
 
-enum class Suit {
-    SPADES, HEARTS, DIAMONDS, CLUBS
-}
+enum class Suit { SPADES, HEARTS, DIAMONDS, CLUBS }
