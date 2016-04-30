d2c430b36c07b48451a3c476743204de5e97f7a5
s-webber <webbers.github@gmail.com>
2016-04-30 11:27:00 +0100

    find the odd numbers solution

--- src/main/kotlin/kotlin4clojure/easy/find_the_odd_numbers/FindTheOddNumbers.kt
+++ src/main/kotlin/kotlin4clojure/easy/find_the_odd_numbers/FindTheOddNumbers.kt
@@ -1,5 +1,3 @@
 package kotlin4clojure.easy.find_the_odd_numbers
 
-fun findOddNumbers(input: Collection<Int>): List<Int> {
-    TODO("Add your solution here")
-}
+fun findOddNumbers(input: Collection<Int>) = input.filter { it % 2 == 1 }
