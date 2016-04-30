c7861223df38e3d44f83a240360b6e39c659a3e1
s-webber <webbers.github@gmail.com>
2016-04-30 13:34:53 +0100

    group list solution

--- src/main/kotlin/kotlin4clojure/easy/group_a_list/GroupList.kt
+++ src/main/kotlin/kotlin4clojure/easy/group_a_list/GroupList.kt
@@ -1,5 +1,3 @@
 package kotlin4clojure.easy.group_a_list
 
-fun <K, V> groupList(function: (V) -> K, input: List<V>): Map<K, List<V>> {
-    TODO("Add your solution here")
-}
+fun <K, V> groupList(f: (V) -> K, input: List<V>) = input.groupBy(f)
