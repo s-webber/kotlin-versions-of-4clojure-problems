fe7f88f4ae821cedde527a603c2d162fb7e4bddf
s-webber <webbers.github@gmail.com>
2016-04-30 16:17:41 +0100

    half truth solution

--- src/main/kotlin/kotlin4clojure/easy/halftruth/HalfTruth.kt
+++ src/main/kotlin/kotlin4clojure/easy/halftruth/HalfTruth.kt
@@ -1,5 +1,8 @@
 package kotlin4clojure.easy.halftruth
 
 fun halfTruth(vararg input: Boolean): Boolean {
-    TODO("Add your solution here")
+    var someTrue = false;
+    var someFalse = false;
+    input.forEach { if (it) someTrue = true else someFalse = true }
+    return someTrue && someFalse
 }
