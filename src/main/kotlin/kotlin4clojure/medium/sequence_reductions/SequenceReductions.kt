7540b770206b2f4e5e321634544e77430242cc18
s-webber <webbers.github@gmail.com>
2016-05-01 11:01:06 +0100

    tidy-up

--- src/main/kotlin/kotlin4clojure/medium/sequence_reductions/SequenceReductions.kt
+++ src/main/kotlin/kotlin4clojure/medium/sequence_reductions/SequenceReductions.kt
@@ -1,9 +1,9 @@
 package kotlin4clojure.medium.sequence_reductions
 
-fun <T> sequenceReductions(f: (T, T) -> T, input: Sequence<T>): Sequence<T> {
+fun <T: Any> sequenceReductions(f: (T, T) -> T, input: Sequence<T>): Sequence<T> {
     return sequenceReductions(f, input.first(), input.drop(1))
 }
 
-fun <R, A> sequenceReductions(f: (R, A) -> R, start: R, input: Sequence<A>): Sequence<R> {
+fun <R: Any, A> sequenceReductions(f: (R, A) -> R, start: R, input: Sequence<A>): Sequence<R> {
     TODO("Add your solution here")
 }
