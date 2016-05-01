12a404a2832d34dd448399be9825a45cb196b2fc
s-webber <webbers.github@gmail.com>
2016-05-01 11:18:27 +0100

    duplicate sequence solution

--- src/main/kotlin/kotlin4clojure/easy/duplicate_a_sequence/DuplicateSequence.kt
+++ src/main/kotlin/kotlin4clojure/easy/duplicate_a_sequence/DuplicateSequence.kt
@@ -1,5 +1,18 @@
 package kotlin4clojure.easy.duplicate_a_sequence
 
 fun <T: Any> duplicateSequence(input: Sequence<T>): Sequence<T> {
-    TODO("Add your solution here")
+    var remaining = input
+    var duplicate = false
+
+    fun next(previous: T): T? {
+        duplicate = !duplicate
+        if (duplicate) {
+            return previous
+        } else {
+            remaining = remaining.drop(1)
+            return remaining.firstOrNull()
+        }
+    }
+
+    return generateSequence(input.firstOrNull(), ::next)
 }
