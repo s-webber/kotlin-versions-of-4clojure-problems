061b4ab692e91d9935f57bc3b1000aa107f4a23a
s-webber <webbers.github@gmail.com>
2016-05-01 14:30:52 +0100

    function composition solution

--- src/main/kotlin/kotlin4clojure/medium/function_composition/FunctionComposition.kt
+++ src/main/kotlin/kotlin4clojure/medium/function_composition/FunctionComposition.kt
@@ -1,5 +1,5 @@
 package kotlin4clojure.medium.function_composition
 
 operator fun <X, Y, Z> Function1<X, Y>.plus(y: (Y) -> Z): (X) -> Z {
-    TODO("Add your solution here")
+    return { y.invoke(this.invoke(it)) }
 }
