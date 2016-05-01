58b5dc3ca987b211450b70f88842009cd1ded190
s-webber <webbers.github@gmail.com>
2016-05-01 16:37:14 +0100

    dot product solution

--- src/main/kotlin/kotlin4clojure/easy/dot_product/DotProduct.kt
+++ src/main/kotlin/kotlin4clojure/easy/dot_product/DotProduct.kt
@@ -1,5 +1,3 @@
 package kotlin4clojure.easy.dot_product
 
-fun dotProduct(a: Array<Int>, b: Array<Int>): Int {
-    TODO("Add your solution here")
-}
+fun dotProduct(a: Array<Int>, b: Array<Int>) = a.mapIndexed { i, t -> t * b[i] }.sum()
