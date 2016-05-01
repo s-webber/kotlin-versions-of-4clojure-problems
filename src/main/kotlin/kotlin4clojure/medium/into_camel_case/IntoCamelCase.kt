421dd7ec1c29a3495f93bbb4aa9c8e21a67ce373
s-webber <webbers.github@gmail.com>
2016-05-01 17:02:31 +0100

    into camel case solution

--- src/main/kotlin/kotlin4clojure/medium/into_camel_case/IntoCamelCase.kt
+++ src/main/kotlin/kotlin4clojure/medium/into_camel_case/IntoCamelCase.kt
@@ -1,5 +1,15 @@
 package kotlin4clojure.medium.into_camel_case
 
 fun intoCamelCase(input: String): String {
-    TODO("Add your solution here")
+    val sb = StringBuilder()
+    var toUpper = false
+    for (c in input) {
+        if (c == '-') {
+            toUpper = true
+        } else  {
+            sb.append(if (toUpper) c.toUpperCase() else c)
+            toUpper = false
+        }
+    }
+    return sb.toString()
 }
