3143fd2c87ae1414ac2e8f9202ae08cdb6aafa75
s-webber <webbers.github@gmail.com>
2016-05-01 11:20:29 +0100

    replicate a sequence solution

--- src/main/kotlin/kotlin4clojure/easy/replicate_a_sequence/ReplicateSequence.kt
+++ src/main/kotlin/kotlin4clojure/easy/replicate_a_sequence/ReplicateSequence.kt
@@ -1,5 +1,19 @@
 package kotlin4clojure.easy.replicate_a_sequence
 
 fun <T: Any> replicateSequence(input: Sequence<T>, n: Int): Sequence<T> {
-    TODO("Add your solution here")
+    var remaining = input
+    var duplicateCtr = 0
+
+    fun next(previous: T): T? {
+        duplicateCtr++
+        if (duplicateCtr < n) {
+            return previous
+        } else {
+            duplicateCtr = 0
+            remaining = remaining.drop(1)
+            return remaining.firstOrNull()
+        }
+    }
+
+    return generateSequence(input.firstOrNull(), ::next)
 }
