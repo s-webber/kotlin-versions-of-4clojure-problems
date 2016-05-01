df69b76da945c92779f6f0c32af45d36f65bbd59
s-webber <webbers.github@gmail.com>
2016-05-01 11:11:00 +0100

    compress sequence solution

--- src/main/kotlin/kotlin4clojure/easy/compress_a_sequence/CompressSequence.kt
+++ src/main/kotlin/kotlin4clojure/easy/compress_a_sequence/CompressSequence.kt
@@ -1,5 +1,12 @@
 package kotlin4clojure.easy.compress_a_sequence
 
 fun <T : Any> compressSequence(input: Sequence<T>): Sequence<T> {
-    TODO("Add your solution here")
+    var remaining = input
+
+    fun next(previous: T): T? {
+        remaining = remaining.dropWhile { it == previous }
+        return remaining.firstOrNull()
+    }
+
+    return generateSequence(input.firstOrNull(), ::next)
 }
