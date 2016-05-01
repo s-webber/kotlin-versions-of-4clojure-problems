97496c26a6aa8177e92e1c49b3728c727e7e30e0
s-webber <webbers.github@gmail.com>
2016-05-01 19:14:52 +0100

    prime numbers solution

--- src/main/kotlin/kotlin4clojure/medium/prime_numbers/PrimeNumbers.kt
+++ src/main/kotlin/kotlin4clojure/medium/prime_numbers/PrimeNumbers.kt
@@ -1,5 +1,10 @@
 package kotlin4clojure.medium.prime_numbers
 
+import java.math.BigInteger
+
 fun primeNumbers(n: Int): List<Int> {
-    TODO("Add your solution here")
+    val s = generateSequence(BigInteger("2"), { it.nextProbablePrime() });
+    return s.take(n)
+            .map { it.intValueExact() }
+            .toList()
 }
