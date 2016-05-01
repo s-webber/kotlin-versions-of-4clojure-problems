b7abf7f20469c950705a96e6904acac75881409a
s-webber <webbers.github@gmail.com>
2016-05-01 13:58:09 +0100

    global take while solution

--- src/main/kotlin/kotlin4clojure/medium/global_takewhile/GlobalTakeWhile.kt
+++ src/main/kotlin/kotlin4clojure/medium/global_takewhile/GlobalTakeWhile.kt
@@ -1,5 +1,26 @@
 package kotlin4clojure.medium.global_takewhile
 
 fun <T: Any> globalTakeWhile(n: Int, predicate: (T) -> Boolean, input: List<T>): Sequence<T> {
-    TODO("Add your solution here")
+    var remaining = input
+    var matches = 0
+
+    fun next(): T? {
+        val next = remaining.firstOrNull()
+        if (next == null) {
+            return null
+        } else {
+            if (predicate.invoke(next)) {
+                matches++
+            }
+
+            if (matches < n) {
+                remaining = remaining.drop(1)
+                return next
+            } else {
+                return null
+            }
+        }
+    }
+
+    return generateSequence(::next)
 }
