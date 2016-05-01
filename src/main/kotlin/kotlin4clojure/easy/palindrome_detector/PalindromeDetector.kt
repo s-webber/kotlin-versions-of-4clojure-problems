c507fe382fbb59b7ffa46aeca838b0ed51379b36
s-webber <webbers.github@gmail.com>
2016-05-01 16:12:12 +0100

    palindrome detector solution

--- src/main/kotlin/kotlin4clojure/easy/palindrome_detector/PalindromeDetector.kt
+++ src/main/kotlin/kotlin4clojure/easy/palindrome_detector/PalindromeDetector.kt
@@ -1,5 +1,12 @@
 package kotlin4clojure.easy.palindrome_detector
 
 fun palindromeDetector(vararg input: Any): Boolean {
-    TODO("Add your solution here")
+    val maxIdx = input.size - 1
+    val middle = input.size / 2
+    for (i in 0..middle) {
+        if (input[i] != input[maxIdx - i]) {
+            return false;
+        }
+    }
+    return true;
 }
