8ef306aa40363789e721b4bd2785e7d5417a42f7
s-webber <webbers.github@gmail.com>
2016-05-01 11:10:40 +0100

    reimplement iterate solution

--- src/main/kotlin/kotlin4clojure/easy/reimplement_iterate/ReimplementIterate.kt
+++ src/main/kotlin/kotlin4clojure/easy/reimplement_iterate/ReimplementIterate.kt
@@ -1,5 +1,3 @@
 package kotlin4clojure.easy.reimplement_iterate
 
-fun <T: Any> reimplementIterate(f: (T) -> T, start: T): Sequence<T> {
-    TODO("Add your solution here")
-}
+fun <T: Any> reimplementIterate(f: (T) -> T, start: T) = generateSequence(start, { f.invoke(it) })
