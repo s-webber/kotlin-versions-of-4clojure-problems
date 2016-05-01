d86f909c2f46d0a96f25877f2bf9d5517a170f04
s-webber <webbers.github@gmail.com>
2016-05-01 11:09:44 +0100

    fibonacci sequence solution

--- src/main/kotlin/kotlin4clojure/easy/fibonacci_sequence/FibonacciSequence.kt
+++ src/main/kotlin/kotlin4clojure/easy/fibonacci_sequence/FibonacciSequence.kt
@@ -1,5 +1,19 @@
 package kotlin4clojure.easy.fibonacci_sequence
 
-fun fibonacciSequence(input: Int): List<Long> {
-    TODO("Add your solution here")
+fun fibonacciSequence(n: Int): List<Long> {
+    return return fibonacciSequence().take(n).toList()
+}
+
+fun fibonacciSequence() : Sequence<Long> {
+    var a = 0L
+    var b = 1L
+
+    fun next() : Long {
+        val result = a + b
+        a = b
+        b = result
+        return a
+    }
+
+    return generateSequence(::next)
 }
