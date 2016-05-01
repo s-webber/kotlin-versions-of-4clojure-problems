d602b8e16ec26b951d424869fed6374f956312e6
s-webber <webbers.github@gmail.com>
2016-05-01 14:17:55 +0100

    count occurrences solution

--- src/main/kotlin/kotlin4clojure/medium/count_occurrences/CountOccurrences.kt
+++ src/main/kotlin/kotlin4clojure/medium/count_occurrences/CountOccurrences.kt
@@ -1,5 +1,5 @@
 package kotlin4clojure.medium.count_occurrences
 
 fun <T> countOccurrences(input: List<T>): Map<T, Int> {
-    TODO("Add your solution here")
+    return input.groupBy { it }.mapValues { it.value.size }
 }
