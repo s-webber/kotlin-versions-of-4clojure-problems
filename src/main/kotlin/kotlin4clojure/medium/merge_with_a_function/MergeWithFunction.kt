ac8967397a4c3a818ccf61fdb9424d4e60c4bd1f
s-webber <webbers.github@gmail.com>
2016-05-01 13:33:37 +0100

    merge with function solution

--- src/main/kotlin/kotlin4clojure/medium/merge_with_a_function/MergeWithFunction.kt
+++ src/main/kotlin/kotlin4clojure/medium/merge_with_a_function/MergeWithFunction.kt
@@ -1,5 +1,16 @@
 package kotlin4clojure.medium.merge_with_a_function
 
 fun <K, V> mergeWithFunction(f: (V, V) -> V, vararg maps: Map<K, V>) : Map<K, V> {
-    TODO("Add your solution here")
+    return maps.reduce { a, b ->
+        (a.keys + b.keys).map {
+            val aValue = a[it]
+            val bValue = b[it]
+            val combinedValue = when {
+                aValue == null -> bValue!!
+                bValue == null -> aValue
+                else -> f.invoke(aValue, bValue)
+            }
+            Pair(it, combinedValue)
+        }.toMap()
+    }
 }
