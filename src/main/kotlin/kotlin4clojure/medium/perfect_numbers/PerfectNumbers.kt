ef5ca3a159ad23b4fafdb38972700332c6780da2
s-webber <webbers.github@gmail.com>
2016-05-01 16:30:19 +0100

    perfect numbers solution

--- src/main/kotlin/kotlin4clojure/medium/perfect_numbers/PerfectNumbers.kt
+++ src/main/kotlin/kotlin4clojure/medium/perfect_numbers/PerfectNumbers.kt
@@ -1,5 +1,13 @@
 package kotlin4clojure.medium.perfect_numbers
 
 fun perfectNumber(input: Int): Boolean {
-    TODO("Add your solution here")
+    var n = 2
+    var result = 1
+    while (n < input) {
+        if (input % n == 0) {
+            result += n
+        }
+        n++
+    }
+    return input == result
 }
