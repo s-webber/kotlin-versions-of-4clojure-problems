bac4c0c3a875b4c76d873f074df584515aa1217c
s-webber <webbers.github@gmail.com>
2016-05-01 14:09:05 +0100

    pack sequence solution

--- src/main/kotlin/kotlin4clojure/easy/pack_a_sequence/PackSequence.kt
+++ src/main/kotlin/kotlin4clojure/easy/pack_a_sequence/PackSequence.kt
@@ -1,5 +1,18 @@
 package kotlin4clojure.easy.pack_a_sequence
 
 fun <T: Any> packSequence(input: Sequence<T>): Sequence<List<T>> {
-    TODO("Add your solution here")
+    var remaining = input
+
+    fun next(): List<T>? {
+        val value = remaining.firstOrNull()
+        if (value == null) {
+            return null
+        } else {
+            val result = remaining.takeWhile { it == value }.toList()
+            remaining = remaining.drop(result.size)
+            return result
+        }
+    }
+
+    return generateSequence(::next)
 }
