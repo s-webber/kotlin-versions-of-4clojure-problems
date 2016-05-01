686e876f243e11d638596e7ccbafebc79d988e37
s-webber <webbers.github@gmail.com>
2016-05-01 13:38:56 +0100

    oscilrate  solution

--- src/main/kotlin/kotlin4clojure/medium/oscilrate/Oscilrate.kt
+++ src/main/kotlin/kotlin4clojure/medium/oscilrate/Oscilrate.kt
@@ -1,5 +1,15 @@
 package kotlin4clojure.medium.oscilrate
 
 fun <T: Any> oscilrate(initialValue: T, vararg functions: (T) -> T): Sequence<T> {
-    TODO("Add your solution here")
+    var i = 0
+
+    fun next(previous: T): T? {
+        val f = functions[i++]
+        if (i == functions.size) {
+            i = 0
+        }
+        return f.invoke(previous)
+    }
+
+    return generateSequence(initialValue, ::next)
 }
