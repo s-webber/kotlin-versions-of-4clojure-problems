47a0d9873c1f7d346a82423334e03e91ce2d9220
s-webber <webbers.github@gmail.com>
2016-05-01 16:46:00 +0100

    equivalence classes solution

--- src/main/kotlin/kotlin4clojure/medium/equivalence_classes/EquivalenceClasses.kt
+++ src/main/kotlin/kotlin4clojure/medium/equivalence_classes/EquivalenceClasses.kt
@@ -1,5 +1,8 @@
 package kotlin4clojure.medium.equivalence_classes
 
 fun <X, Y> equivalenceClasses(f: (X) -> Y, input: Set<X>): Set<Set<X>> {
-    TODO("Add your solution here")
+    return input.groupBy(f)
+                .mapValues { it.value.toSet() }
+                .values
+                .toSet()
 }
