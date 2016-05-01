e0039e641d2dd42feef0041e8c7cc6ea11efcfa0
s-webber <webbers.github@gmail.com>
2016-05-01 16:33:32 +0100

    flipping out solution

--- src/main/kotlin/kotlin4clojure/medium/flipping_out/FlippingOut.kt
+++ src/main/kotlin/kotlin4clojure/medium/flipping_out/FlippingOut.kt
@@ -1,5 +1,3 @@
 package kotlin4clojure.medium.flipping_out
 
-fun <X, Y, Z> flip(f: (Y, X) -> Z, a: X, b: Y): Z {
-    TODO("Add your solution here")
-}
+fun <X, Y, Z> flip(f: (Y, X) -> Z, a: X, b: Y) = f.invoke(b, a)
