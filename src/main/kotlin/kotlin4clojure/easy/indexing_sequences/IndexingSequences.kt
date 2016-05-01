b3c90d2e6dde1e99302beb8db234909d89b48e55
s-webber <webbers.github@gmail.com>
2016-05-01 14:58:52 +0100

    indexing sequences solution

--- src/main/kotlin/kotlin4clojure/easy/indexing_sequences/IndexingSequences.kt
+++ src/main/kotlin/kotlin4clojure/easy/indexing_sequences/IndexingSequences.kt
@@ -1,5 +1,5 @@
 package kotlin4clojure.easy.indexing_sequences
 
 fun <T: Any> indexingSequences(input: Sequence<T>): Sequence<Pair<T, Int>> {
-    TODO("Add your solution here")
+    return input.mapIndexed { i, t -> Pair(t, i) }
 }
