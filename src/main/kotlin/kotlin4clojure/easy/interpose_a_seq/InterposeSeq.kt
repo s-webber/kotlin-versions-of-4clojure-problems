bae5820c95fc877f03359002dc458ca05d7837de
s-webber <webbers.github@gmail.com>
2016-05-02 10:38:30 +0100

    interpose a seq solution

--- src/main/kotlin/kotlin4clojure/easy/interpose_a_seq/InterposeSeq.kt
+++ src/main/kotlin/kotlin4clojure/easy/interpose_a_seq/InterposeSeq.kt
@@ -1,5 +1,23 @@
 package kotlin4clojure.easy.interpose_a_seq
 
 fun <T: Any> interposeSeq(separator: T, input: Sequence<T>): Sequence<T> {
-    TODO("Add your solution here")
+    var remaining = input
+    var interpose = true
+
+    fun next(): T? {
+        val next = remaining.firstOrNull()
+        if (next == null) {
+            return null
+        }
+
+        interpose = !interpose
+        if (interpose) {
+            return separator
+        } else {
+            remaining = remaining.drop(1)
+            return next
+        }
+    }
+
+    return generateSequence(::next)
 }
