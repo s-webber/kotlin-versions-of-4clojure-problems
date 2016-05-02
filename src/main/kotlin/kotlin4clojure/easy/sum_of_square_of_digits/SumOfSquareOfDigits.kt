f4c214b82f09f408490c4e08900f3709c4ed14a0
s-webber <webbers.github@gmail.com>
2016-05-02 09:50:33 +0100

    sum of square of digits solution

--- src/main/kotlin/kotlin4clojure/easy/sum_of_square_of_digits/SumOfSquareOfDigits.kt
+++ src/main/kotlin/kotlin4clojure/easy/sum_of_square_of_digits/SumOfSquareOfDigits.kt
@@ -1,5 +1,23 @@
 package kotlin4clojure.easy.sum_of_square_of_digits
 
 fun sumOfSquareOfDigits(input: IntRange): Int {
-    TODO("Add your solution here")
+    return input.count(::isLessThanSumOfSquareOfDigits)
+}
+
+fun isLessThanSumOfSquareOfDigits(input: Int): Boolean {
+    val sumOfSquareOfDigits = toDigits(input).map { it * it }.sum()
+    return input < sumOfSquareOfDigits
+}
+
+fun toDigits(input: Int): Sequence<Int> {
+    var n = input
+    return generateSequence({
+        if (n > 0) {
+            val result = n % 10
+            n = n / 10
+            result
+        } else {
+            null
+        }
+    })
 }
