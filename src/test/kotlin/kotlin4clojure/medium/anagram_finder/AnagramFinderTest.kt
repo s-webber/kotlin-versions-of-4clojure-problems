c7beaf549c31dbb1f1eaa7e87d7ed7b15e2c99f5
s-webber <webbers.github@gmail.com>
2016-05-02 10:05:39 +0100

    tidy-up

--- src/test/kotlin/kotlin4clojure/medium/anagram_finder/AnagramFinderTest.kt
+++ src/test/kotlin/kotlin4clojure/medium/anagram_finder/AnagramFinderTest.kt
@@ -6,7 +6,7 @@ import org.junit.Test
 /**
  * 77. Anagram Finder
  * 
- * Write a function which finds all the anagrams in a vector of words.
+ * Write a function which finds all the anagrams in an array of words.
  * A word x is an anagram of word y if all the letters in x can be rearranged in a different order to form y.
  * Your function should return a set of sets, where each sub-set is a group of words which are anagrams of each other.
  * Each sub-set should have at least two words. Words without any anagrams should not be included in the result.
