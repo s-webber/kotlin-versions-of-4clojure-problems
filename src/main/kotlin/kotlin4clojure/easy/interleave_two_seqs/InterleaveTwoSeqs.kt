caf8082561576d5dc6b9a7af8787e0e4190801d5
s-webber <webbers.github@gmail.com>
2016-05-02 10:53:10 +0100

    interleave two seqs solution

--- src/main/kotlin/kotlin4clojure/easy/interleave_two_seqs/InterleaveTwoSeqs.kt
+++ src/main/kotlin/kotlin4clojure/easy/interleave_two_seqs/InterleaveTwoSeqs.kt
@@ -1,5 +1,24 @@
 package kotlin4clojure.easy.interleave_two_seqs
 
 fun <T: Any> interleaveTwoSequences(a: Sequence<T>, b: Sequence<T>): Sequence<T> {
-    TODO("Add your solution here")
+    var remainingA = a
+    var remainingB = b
+    var selectFromA = false
+
+    fun next(): T? {
+        selectFromA = !selectFromA
+
+        val nextB = remainingB.firstOrNull()
+
+        if (selectFromA && nextB != null) {
+            val nextA = remainingA.firstOrNull()
+            remainingA = remainingA.drop(1)
+            return nextA
+        } else {
+            remainingB = remainingB.drop(1)
+            return nextB
+        }
+    }
+
+    return generateSequence(::next)
 }
