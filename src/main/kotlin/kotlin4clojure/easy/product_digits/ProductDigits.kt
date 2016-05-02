c16ae17f434aeac247f1262e7275c9c39d0b94dd
s-webber <webbers.github@gmail.com>
2016-05-02 13:36:12 +0100

    product digits solution

--- src/main/kotlin/kotlin4clojure/easy/product_digits/ProductDigits.kt
+++ src/main/kotlin/kotlin4clojure/easy/product_digits/ProductDigits.kt
@@ -1,5 +1,14 @@
 package kotlin4clojure.easy.product_digits
 
 fun productDigits(a: Int, b: Int): List<Int> {
-    TODO("Add your solution here")
+    var number = a * b
+    return generateSequence({
+        if (number > 0) {
+            val result = number % 10
+            number /= 10
+            result
+        } else {
+            null
+        }
+    }).toList().asReversed()
 }
