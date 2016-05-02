8a751603898bff4845b0e2a1dc3eb5fa4b581ef6
s-webber <webbers.github@gmail.com>
2016-05-02 11:53:56 +0100

    write roman numerals solution

--- src/main/kotlin/kotlin4clojure/medium/write_roman_numerals/WriteRomanNumerals.kt
+++ src/main/kotlin/kotlin4clojure/medium/write_roman_numerals/WriteRomanNumerals.kt
@@ -1,5 +1,33 @@
 package kotlin4clojure.medium.write_roman_numerals
 
 fun writeRomanNumerals(input: Int): String {
-    TODO("Add your solution here")
+    val output = StringBuilder()
+    writeRomanNumerals(input, output)
+    return output.toString()
 }
+
+tailrec fun writeRomanNumerals(input: Int, output: StringBuilder) {
+    if (input != 0) {
+        val next = next(input)
+        output.append(next.first)
+        writeRomanNumerals(input - next.second, output)
+    }
+}
+
+fun next(input: Int): Pair<String, Int> {
+    return romanNumerals.first { it.second <= input }
+}
+
+val romanNumerals = listOf("M" to 1000,
+                           "CM" to 900,
+                           "D" to 500,
+                           "CD" to 400,
+                           "C" to 100,
+                           "XC" to 90,
+                           "L" to 50,
+                           "XL" to 40,
+                           "X" to 10,
+                           "IX" to 9,
+                           "V" to 5,
+                           "IV" to 4,
+                           "I" to 1)
