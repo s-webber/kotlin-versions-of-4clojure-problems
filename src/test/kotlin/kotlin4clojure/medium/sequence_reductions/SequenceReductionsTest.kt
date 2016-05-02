4615456860e30998a66513a0785bb847aa7940ef
s-webber <webbers.github@gmail.com>
2016-05-02 11:38:18 +0100

    corrected sequence reductions test

--- src/test/kotlin/kotlin4clojure/medium/sequence_reductions/SequenceReductionsTest.kt
+++ src/test/kotlin/kotlin4clojure/medium/sequence_reductions/SequenceReductionsTest.kt
@@ -12,7 +12,7 @@ import org.junit.Test
 class SequenceReductionsTest {
     @Test fun f1() {
         val expected = listOf(0, 1, 3, 6, 10)
-        val actual = sequenceReductions({a, b -> a + b}, generateSequence(0, {it + 1})).toList()
+        val actual = sequenceReductions({a, b -> a + b}, generateSequence(0, {it + 1})).take(5).toList()
         assertEquals(expected, actual)
     }
 
