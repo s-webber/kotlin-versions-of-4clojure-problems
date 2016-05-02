b3cbd07f843fbedb0becf1fd0796f623e216f0d2
s-webber <webbers.github@gmail.com>
2016-05-02 11:38:51 +0100

    sequence reductions solutions

--- src/main/kotlin/kotlin4clojure/medium/sequence_reductions/SequenceReductions.kt
+++ src/main/kotlin/kotlin4clojure/medium/sequence_reductions/SequenceReductions.kt
@@ -5,5 +5,17 @@ fun <T: Any> sequenceReductions(f: (T, T) -> T, input: Sequence<T>): Sequence<T>
 }
 
 fun <R: Any, A> sequenceReductions(f: (R, A) -> R, start: R, input: Sequence<A>): Sequence<R> {
-    TODO("Add your solution here")
+    var remaining = input
+
+    fun next(previous: R): R? {
+        val next = remaining.firstOrNull()
+        if (next == null) {
+            return null
+        } else {
+            remaining = remaining.drop(1)
+            return f.invoke(previous, next)
+        }
+    }
+
+    return generateSequence(start, ::next)
 }
