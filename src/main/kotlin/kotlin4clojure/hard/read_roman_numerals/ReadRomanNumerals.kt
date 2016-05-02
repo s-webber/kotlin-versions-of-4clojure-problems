2f795a609b1884a5b87c118f29da0a09c5a03aca
s-webber <webbers.github@gmail.com>
2016-05-02 12:01:30 +0100

    read roman numerals solution

--- src/main/kotlin/kotlin4clojure/hard/read_roman_numerals/ReadRomanNumerals.kt
+++ src/main/kotlin/kotlin4clojure/hard/read_roman_numerals/ReadRomanNumerals.kt
@@ -1,5 +1,29 @@
 package kotlin4clojure.hard.read_roman_numerals
 
-fun readRomanNumerals(input: String): Int {
-    TODO("Add your solution here")
+tailrec fun readRomanNumerals(input: String): Int {
+    if (input.isEmpty()) {
+        return 0
+    } else {
+        val next = next(input)
+        val remainder = input.substring(0, input.length - next.first.length)
+        return next.second + readRomanNumerals(remainder)
+    }
 }
+
+fun next(input: String): Pair<String, Int> {
+    return romanNumerals.first { input.endsWith(it.first) }
+}
+
+val romanNumerals = listOf("CM" to 900,
+                           "CD" to 400,
+                           "XC" to 90,
+                           "XL" to 40,
+                           "IX" to 9,
+                           "IV" to 4,
+                           "M" to 1000,
+                           "D" to 500,
+                           "C" to 100,
+                           "L" to 50,
+                           "X" to 10,
+                           "V" to 5,
+                           "I" to 1)
