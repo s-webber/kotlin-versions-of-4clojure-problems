70c9e4db097b2e89916057354e777e3eff6683a1
s-webber <webbers.github@gmail.com>
2016-05-02 13:36:38 +0100

    the balance of n solution

--- src/main/kotlin/kotlin4clojure/medium/the_balance_of_n/TheBalanceOfN.kt
+++ src/main/kotlin/kotlin4clojure/medium/the_balance_of_n/TheBalanceOfN.kt
@@ -1,5 +1,22 @@
 package kotlin4clojure.medium.the_balance_of_n
 
 fun theBalanceOfN(input: Int): Boolean {
-    TODO("Add your solution here")
+    val digits = input.toDigits().toList()
+    val middle = digits.size / 2
+    val left = digits.take(middle).sum()
+    val right = digits.takeLast(middle).sum()
+    return left == right
+}
+
+fun Int.toDigits(): Sequence<Int> {
+    var n = this
+    return generateSequence({
+        if (n > 0) {
+            val result = n % 10
+            n /= 10
+            result
+        } else {
+            null
+        }
+    })
 }
