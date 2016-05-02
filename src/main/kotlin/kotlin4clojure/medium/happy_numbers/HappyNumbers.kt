08f66bb247f497613dc4eaee00c49dc899b14a3f
s-webber <webbers.github@gmail.com>
2016-05-02 13:52:14 +0100

    happy numbers solution

--- src/main/kotlin/kotlin4clojure/medium/happy_numbers/HappyNumbers.kt
+++ src/main/kotlin/kotlin4clojure/medium/happy_numbers/HappyNumbers.kt
@@ -1,5 +1,23 @@
 package kotlin4clojure.medium.happy_numbers
 
-fun happyNumber(input: Int): Boolean {
-    TODO("Add your solution here")
+tailrec fun happyNumber(input: Int, previous: Set<Int> = emptySet()): Boolean {
+    val sum = input.toDigits().map { it * it }.sum()
+    when {
+        sum == 1 -> return true
+        previous.contains(sum) -> return false
+        else -> return happyNumber(sum, previous + sum)
+    }
+}
+
+fun Int.toDigits(): Sequence<Int> {
+    var n = this
+    return generateSequence({
+        if (n > 0) {
+            val result = n % 10
+            n /= 10
+            result
+        } else {
+            null
+        }
+    })
 }
