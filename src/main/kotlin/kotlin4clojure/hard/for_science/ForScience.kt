938bc5bcf4dbee6f3e1632a23af9c29f01ea0fdf
s-webber <webbers.github@gmail.com>
2016-05-21 11:43:12 +0100

    for science solution

--- src/main/kotlin/kotlin4clojure/hard/for_science/ForScience.kt
+++ src/main/kotlin/kotlin4clojure/hard/for_science/ForScience.kt
@@ -1,5 +1,33 @@
 package kotlin4clojure.hard.for_science
 
 fun forScience(vararg input: String): Boolean {
-    TODO("Add your solution here")
+    val cells = input.mapIndexed { rowIdx, line ->
+        line.mapIndexed { colIdx, cell ->
+            Pair(colIdx to rowIdx, cell)
+        }
+    }.flatten().toMap()
+    val mouses = cells.filterValues { it == 'M' }.keys.toSet()
+    val cheeses = cells.filterValues { it == 'C' }.keys.toSet()
+    val empty = cells.filterValues { it == ' ' }.keys.toSet()
+    return isConnected(mouses, emptySet(), empty + cheeses, cheeses)
 }
+
+ tailrec fun isConnected(toVisit: Set<Pair<Int, Int>>,
+                         visited: Set<Pair<Int, Int>>,
+                         visitable: Set<Pair<Int, Int>>,
+                         goal: Set<Pair<Int, Int>>): Boolean {
+    val next = toVisit.firstOrNull()
+    return when {
+        next == null -> false
+        goal.contains(next) -> true
+        else -> {
+            val newToVisit = (adjacent(next) - visited).intersect(visitable)
+            isConnected(toVisit - next + newToVisit, visited + next, visitable, goal)
+        }
+    }
+}
+
+fun adjacent(cell: Pair<Int, Int>) =
+    moves.map { Pair(cell.first + it.first, cell.second + it.second) }
+
+val moves = listOf(Pair(0, 1), Pair(0, -1), Pair(1, 0), Pair(-1, 0))
