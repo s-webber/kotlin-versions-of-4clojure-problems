fd8012626f5a3c3483e0c0ed3342535bf4714f76
s-webber <webbers.github@gmail.com>
2016-05-21 11:42:48 +0100

    anagram finder solution

--- src/main/kotlin/kotlin4clojure/medium/anagram_finder/AnagramFinder.kt
+++ src/main/kotlin/kotlin4clojure/medium/anagram_finder/AnagramFinder.kt
@@ -2,8 +2,8 @@ package kotlin4clojure.medium.anagram_finder
 
 fun anagrams(vararg keys:String): Set<Set<String>> {
     return keys.groupBy { it.toSortedSet() }
-               .filter { it.value.size > 1 }
-               .mapValues { it.value.toSet() }
                .values
+               .filter { it.size > 1 }
+               .map { it.toSet() }
                .toSet()
 }
