17f2e2f2f5ece58ee052b63c333839c9bc142fd9
s-webber <webbers.github@gmail.com>
2016-05-30 10:43:04 +0100

    graph connectivity solution

--- src/main/kotlin/kotlin4clojure/hard/graph_connectivity/GraphConnectivity.kt
+++ src/main/kotlin/kotlin4clojure/hard/graph_connectivity/GraphConnectivity.kt
@@ -1,5 +1,23 @@
 package kotlin4clojure.hard.graph_connectivity
 
 fun <T> graphConnectivity(input: Set<Pair<T, T>>): Boolean {
-    TODO("Add your solution here")
+    val sets = input.map { setOf(it.first, it.second) }
+    return isConnected(sets.first(), sets.drop(1).toSet())
+}
+
+tailrec fun <T> isConnected(visited: Set<T>, rest: Set<Set<T>>): Boolean {
+    if (rest.isEmpty()) {
+        return true
+    }
+
+    val intersection = rest.filter { visited.containsAny(it) }
+    if (intersection.isEmpty()) {
+        return false
+    }
+
+    return isConnected(visited + intersection.flatten(), rest - intersection)
+}
+
+fun <T> Collection<T>.containsAny(elements: Collection<T>): Boolean {
+    return this.any { elements.contains(it) }
 }
