245140be590b0ce47a7890b09bd839e2fa62124d
s-webber <webbers.github@gmail.com>
2016-05-30 10:51:52 +0100

    tree reparenting solution

--- src/main/kotlin/kotlin4clojure/hard/tree_reparenting/TreeReparenting.kt
+++ src/main/kotlin/kotlin4clojure/hard/tree_reparenting/TreeReparenting.kt
@@ -1,9 +1,37 @@
 package kotlin4clojure.hard.tree_reparenting
 
 fun treeReparenting(id: String, tree: Node): Node? {
-    TODO("Add your solution here")
+    return find(id, tree, emptyList())
+}
+
+fun find(target: String, next: Node, parents: List<Node>): Node? {
+    if (target.equals(next.id)) {
+        if (parents.isEmpty()) {
+            return next
+        } else {
+            return Node(next.id, next.children + restructure(parents.last(), next, parents.dropLast(1)))
+        }
+    } else {
+        val updatedParents = parents + next
+        next.children.forEach {
+            val n = find(target, it, updatedParents)
+            if (n!=null) {
+                return n
+            }
+        }
+        return null
+    }
+}
+
+fun restructure(n: Node, newParent: Node, oldParents: List<Node>): Node {
+    val childrenMinusNewParent = n.children.filterNot { newParent == it }
+    if (oldParents.isEmpty()) {
+        return Node(n.id, childrenMinusNewParent)
+    } else {
+        return Node(n.id, childrenMinusNewParent + restructure(oldParents.first(), n, oldParents.drop(1)))
+    }
 }
 
 fun treeOf(id: String, vararg children: Node) = Node(id, children.asList())
 
-data class Node (val id: String, val children: List<Node>? = null)
+data class Node (val id: String, val children: List<Node> = emptyList())
