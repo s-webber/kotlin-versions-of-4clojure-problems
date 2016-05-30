fbd98df9a276e27c8726ab0554e8a619dfbc9c91
s-webber <webbers.github@gmail.com>
2016-05-30 11:57:30 +0100

    parentheses again solution

--- src/main/kotlin/kotlin4clojure/medium/parentheses_again/ParenthesesAgain.kt
+++ src/main/kotlin/kotlin4clojure/medium/parentheses_again/ParenthesesAgain.kt
@@ -1,5 +1,30 @@
 package kotlin4clojure.medium.parentheses_again
 
 fun parenthesesAgain(size: Int): Set<String> {
-    TODO("Add your solution here")
+    return recursiveSolve(size, mapOf(0 to setOf("")))
+}
+
+tailrec fun recursiveSolve(target: Int, cache: Map<Int, Set<String>>): Set<String>  {
+    val current = cache.size
+    if (target < current) {
+        return cache[target]!!
+    } else {
+        val updatedCache = cache + Pair(current, calculateNextPermutations(cache))
+        return recursiveSolve(target, updatedCache)
+    }
+}
+
+fun calculateNextPermutations(cache: Map<Int, Set<String>>): Set<String> {
+    val current = cache.size
+    val m = cache[current - 1]!!.map { "($it)" }.toSet()
+    return (1..current - 1).fold(m, { initial, operation ->
+        initial + combine(cache[operation]!!, cache[current - operation]!!)
+    })
+}
+
+fun combine(a: Set<String>, b: Set<String>): Set<String> {
+    return a.map {
+        val left = it
+        b.map { left + it }
+    }.flatten().toSet()
 }
