bd9d0d1d2f4301d86914746b6a9152f11fee5231
s-webber <webbers.github@gmail.com>
2016-05-30 10:45:30 +0100

    number maze solution

--- src/main/kotlin/kotlin4clojure/hard/number_maze/NumberMaze.kt
+++ src/main/kotlin/kotlin4clojure/hard/number_maze/NumberMaze.kt
@@ -1,5 +1,34 @@
 package kotlin4clojure.hard.number_maze
 
 fun numberMaze(start: Int, end: Int): Int {
-    TODO("Add your solution here")
+    return if (start == end) {
+        return 1
+    } else {
+        return numberMaze(setOf(start), end, 1)
+    }
 }
+
+tailrec fun numberMaze(input: Set<Int?>, target: Int, ctr: Int): Int {
+    val next = input.map(::next).flatten().toSet()
+    if (next.any { it == target }) {
+        return ctr + 1
+    } else {
+        return numberMaze(next, target, ctr + 1)
+    }
+}
+
+fun next(i: Int?): List<Int?> {
+    return if (i==null) {
+        emptyList()
+    } else {
+        functions.map { it.invoke(i) }
+    }
+}
+
+val functions = listOf(::double, ::half, ::add2)
+
+fun double(i: Int) = i * 2
+
+fun half(i: Int) = if (i % 2 == 0) i / 2 else null
+
+fun add2(i: Int) = i + 2
