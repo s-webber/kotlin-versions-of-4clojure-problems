83086e2c758748c137bd0c1b0b6346a1bd3dbb6d
s-webber <webbers.github@gmail.com>
2016-07-23 16:21:49 +0100

    word chains solution

--- src/main/kotlin/kotlin4clojure/hard/word_chains/WordChains.kt
+++ src/main/kotlin/kotlin4clojure/hard/word_chains/WordChains.kt
@@ -1,5 +1,44 @@
 package kotlin4clojure.hard.word_chains
 
-fun wordChain(input: Set<String>): Boolean {
-    TODO("Add your solution here")
+fun wordChain(input: Set<String>) =
+    input.any { isChain(it, input - it) }
+
+private fun isChain(current: String, rest: Set<String>) : Boolean =
+    rest.isEmpty() || rest.any { isNeighbourly(current to it) && isChain(it, rest - it) }
+
+private fun isNeighbourly(words: Pair<String, String>) : Boolean {
+    val (first, second) = order(words)
+
+    return when (second.length - first.length) {
+        0 -> isNeighbourlySameLength(first, second)
+        1 -> isNeighbourlyDifferentLength(first, second)
+        else -> false
+    }
+}
+
+private fun order(p : Pair<String, String>) =
+        if (p.first.length > p.second.length) Pair(p.second, p.first) else p
+
+private fun isNeighbourlySameLength(first: String, second: String) : Boolean {
+    assert(first.length == second.length)
+
+    var diffences = 0
+    for (i in first.indices) {
+        if (first[i] != second[i] && ++diffences == 2) {
+            return false
+        }
+    }
+    return true;
+}
+
+private fun isNeighbourlyDifferentLength(first: String, second: String) : Boolean {
+    assert(first.length == second.length - 1)
+
+    var diffences = 0
+    for (i in first.indices) {
+        if (first[i] != second[i + diffences] && ++diffences == 2) {
+            return false
+        }
+    }
+    return true
 }
