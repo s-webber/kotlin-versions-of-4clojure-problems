870e989de9077e629e46bf34aabce9558c7b51ce
s-webber <webbers.github@gmail.com>
2016-07-24 09:39:54 +0100

    partition list now takes a list and returns a sequence of lists

--- src/test/kotlin/kotlin4clojure/medium/partition_a_list/PartitionListTest.kt
+++ src/test/kotlin/kotlin4clojure/medium/partition_a_list/PartitionListTest.kt
@@ -6,24 +6,24 @@ import org.junit.Test
 /**
  * 54. Partition a List
  * 
- * Write a function which returns a list of lists of x items each. Lists of less than x items should not be returned.
+ * Write a function which returns a sequence of lists of x items each. Lists of less than x items should not be returned.
  */
 class PartitionListTest {
     @Test fun f1() {
         val expected = listOf(listOf(0, 1, 2), listOf(3, 4, 5), listOf(6, 7, 8))
-        val actual = partitionList(3, 0..8)
-        assertEquals(expected, actual)
+        val actual = partitionList(3, (0..8).toList())
+        assertEquals(expected, actual.toList())
     }
 
     @Test fun f2() {
         val expected = listOf(listOf(0, 1), listOf(2, 3), listOf(4, 5), listOf(6, 7))
-        val actual = partitionList(2, 0..7)
-        assertEquals(expected, actual)
+        val actual = partitionList(2, (0..7).toList())
+        assertEquals(expected, actual.toList())
     }
 
     @Test fun f3() {
         val expected = listOf(listOf(0, 1, 2), listOf(3, 4, 5))
-        val actual = partitionList(3, 0..7)
-        assertEquals(expected, actual)
+        val actual = partitionList(3, (0..7).toList())
+        assertEquals(expected, actual.toList())
     }
 }
