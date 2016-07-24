00e7fe0b3735f864d48c445ab14b4b43d7e2b0a1
s-webber <webbers.github@gmail.com>
2016-07-24 09:46:26 +0100

    partition list solution

--- src/main/kotlin/kotlin4clojure/medium/partition_a_list/PartitionList.kt
+++ src/main/kotlin/kotlin4clojure/medium/partition_a_list/PartitionList.kt
@@ -1,5 +1,17 @@
 package kotlin4clojure.medium.partition_a_list
 
 fun <T> partitionList(size: Int, input: List<T>): Sequence<List<T>> {
-    TODO("Add your solution here")
+    var remaining = input
+
+    fun next() : List<T>? {
+        if (remaining.size < size) {
+            return null
+        } else {
+            val result = remaining.take(size)
+            remaining = remaining.drop(size)
+            return result
+        }
+    }
+
+    return generateSequence(::next)
 }
