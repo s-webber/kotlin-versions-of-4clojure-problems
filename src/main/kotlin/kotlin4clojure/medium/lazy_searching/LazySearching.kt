7719e70f17a30f923dcb49b9b35db34c8c335b36
s-webber <webbers.github@gmail.com>
2016-07-24 13:38:18 +0100

    lazy searching solution

--- src/main/kotlin/kotlin4clojure/medium/lazy_searching/LazySearching.kt
+++ src/main/kotlin/kotlin4clojure/medium/lazy_searching/LazySearching.kt
@@ -1,5 +1,21 @@
 package kotlin4clojure.medium.lazy_searching
 
-fun lazySearching(input: List<Sequence<Int>>): Int? {
-    TODO("Add your solution here")
+tailrec fun lazySearching(input: List<Sequence<Int>>): Int? {
+    val max = maxOrNull(input)
+    return when {
+        max == null -> null
+        input.all { it.first() == max } -> max
+        else -> lazySearching(input.map { it.dropWhile { it < max } })
+    }
+}
+
+private fun maxOrNull(input: List<Sequence<Int>>): Int? {
+    return input.map {
+        val next = it.firstOrNull()
+        if (next == null) {
+            return null
+        } else {
+            next
+        }
+    }.max()
 }
