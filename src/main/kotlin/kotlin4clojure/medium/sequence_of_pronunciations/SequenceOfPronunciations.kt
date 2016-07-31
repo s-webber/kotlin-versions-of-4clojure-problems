9d4e86626ce2b021bc783fea2514acfaf8a67434
s-webber <webbers.github@gmail.com>
2016-07-31 11:35:57 +0100

    sequence of pronunciations solution

--- src/main/kotlin/kotlin4clojure/medium/sequence_of_pronunciations/SequenceOfPronunciations.kt
+++ src/main/kotlin/kotlin4clojure/medium/sequence_of_pronunciations/SequenceOfPronunciations.kt
@@ -1,5 +1,18 @@
 package kotlin4clojure.medium.sequence_of_pronunciations
 
-fun sequenceOfPronunciations(input: List<Int>): Sequence<List<Int>> {
-    TODO("Add your solution here")
+fun sequenceOfPronunciations(input: List<Int>) =
+    generateSequence(next(input), ::next)
+
+fun next(input: List<Int>) =
+    input.partitionBy { x, y -> x == y }.map { listOf(it.size, it.first()) }.flatten()
+
+fun <T> List<T>.partitionBy(predicate: (T, T) -> Boolean): List<List<T>> {
+    return this.fold(mutableListOf<MutableList<T>>(), { s, e ->
+        if (s.isEmpty() || !predicate(s.last().last(), e)) {
+            s.add(mutableListOf(e))
+        } else {
+            s.last().add(e)
+        }
+        s
+    })
 }
