a21c3a8b8e57624f97d6bbbc7f2b54d26ab4c81b
s-webber <webbers.github@gmail.com>
2016-07-31 09:27:27 +0100

    longest increasing sub-sequence solution

--- src/main/kotlin/kotlin4clojure/hard/longest_increasing_subseq/LongestIncreasingSubSeq.kt
+++ src/main/kotlin/kotlin4clojure/hard/longest_increasing_subseq/LongestIncreasingSubSeq.kt
@@ -1,5 +1,17 @@
 package kotlin4clojure.hard.longest_increasing_subseq
 
 fun longestIncreasingSubSeq(input: List<Int>): List<Int> {
-    TODO("Add your solution here")
+    var longestSubSeq = largest(
+        input.fold(Pair(emptyList<Int>(), emptyList<Int>()), { s, e ->
+            if (s.second.isEmpty() || s.second.last() == e - 1) {
+                Pair(s.first, s.second + e)
+            } else {
+                Pair(largest(s), listOf(e))
+            }
+        })
+     )
+
+    return if (longestSubSeq.size > 1) longestSubSeq else emptyList()
 }
+
+fun <T> largest(p: Pair<List<T>, List<T>>) = if (p.first.size >= p.second.size) p.first else p.second
