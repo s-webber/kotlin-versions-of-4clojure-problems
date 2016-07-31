4c3418524d30355ca1c0f1fec9cd4b78931eb7fc
s-webber <webbers.github@gmail.com>
2016-07-31 09:39:09 +0100

    intervals solution

--- src/main/kotlin/kotlin4clojure/medium/intervals/Intervals.kt
+++ src/main/kotlin/kotlin4clojure/medium/intervals/Intervals.kt
@@ -1,5 +1,23 @@
 package kotlin4clojure.medium.intervals
 
 fun intervals(input: List<Int>): Sequence<Pair<Int, Int>> {
-    TODO("Add your solution here")
+    var remaining = input.sorted()
+    return generateSequence({
+        if (remaining.isEmpty()) {
+            null
+        } else {
+            var previous = remaining.first()
+            val interval = remaining.takeWhile {
+                if (previous + 1 >= it) {
+                    previous = it
+                    true
+                } else {
+                    false
+                }
+            }
+            remaining = remaining.drop(interval.size)
+
+            Pair(interval.first(), interval.last())
+        }
+    })
 }
