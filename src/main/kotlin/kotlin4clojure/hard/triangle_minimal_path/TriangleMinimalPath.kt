3bca8427e7de3c2d9ec266ad14e4d7d62c141732
s-webber <webbers.github@gmail.com>
2016-07-31 09:26:18 +0100

    triangle minimal path solution

--- src/main/kotlin/kotlin4clojure/hard/triangle_minimal_path/TriangleMinimalPath.kt
+++ src/main/kotlin/kotlin4clojure/hard/triangle_minimal_path/TriangleMinimalPath.kt
@@ -1,5 +1,11 @@
 package kotlin4clojure.hard.triangle_minimal_path
 
-fun triangleMinimalPath(input: List<List<Int>>): Int {
-    TODO("Add your solution here")
+fun triangleMinimalPath(input: List<List<Int>>, depth: Int = 0): Int {
+    val value = input[0][depth]
+    val children = input.drop(1)
+    if (children.isEmpty()) {
+        return value
+    } else {
+        return value + Math.min(triangleMinimalPath(children, depth), triangleMinimalPath(children, depth + 1))
+    }
 }
