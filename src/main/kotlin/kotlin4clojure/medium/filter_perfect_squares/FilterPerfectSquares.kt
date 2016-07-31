0a0b8b2356333d89daa3af635c2424bac2f05555
s-webber <webbers.github@gmail.com>
2016-07-31 09:24:57 +0100

    filter perfect squares solution

--- src/main/kotlin/kotlin4clojure/medium/filter_perfect_squares/FilterPerfectSquares.kt
+++ src/main/kotlin/kotlin4clojure/medium/filter_perfect_squares/FilterPerfectSquares.kt
@@ -1,5 +1,4 @@
 package kotlin4clojure.medium.filter_perfect_squares
 
-fun filterPerfectSquares(input:String): String {
-    TODO("Add your solution here")
-}
+fun filterPerfectSquares(input:String) =
+    input.split(',').filter { 0.0 == Math.sqrt(it.toDouble()) % 1 }.joinToString(separator = ",")
