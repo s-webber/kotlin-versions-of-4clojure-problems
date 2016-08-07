1ba8101b8f8dd940d3df399ea24ae43f430966c7
s-webber <webbers.github@gmail.com>
2016-08-07 14:18:26 +0100

    palindromic numbers solution

--- src/main/kotlin/kotlin4clojure/medium/palindromic_numbers/PalindromicNumbers.kt
+++ src/main/kotlin/kotlin4clojure/medium/palindromic_numbers/PalindromicNumbers.kt
@@ -1,5 +1,23 @@
 package kotlin4clojure.medium.palindromic_numbers
 
 fun palindromicNumbers(start: Long): Sequence<Long> {
-    TODO("Add your solution here")
+    var min = 1
+    var max = min * 10
+    var n = 0
+    var even = false
+
+    return generateSequence({
+        if (n == max) {
+            if (even) {
+                min = n
+                max = n * 10
+            } else {
+                n = min
+            }
+            even = !even
+        }
+        val s = n.toString()
+        n++
+        (s + (if (even) s else s.dropLast(1)).reversed()).toLong()
+    }).dropWhile { it < start } // TODO remove dropWhile, set initial value of "n" to "start"
 }
