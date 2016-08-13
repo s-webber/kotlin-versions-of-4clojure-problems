11d6a32eb306a0002b6674defe1043e38d34b770
s-webber <webbers.github@gmail.com>
2016-08-13 15:59:57 +0100

    palindromic numbers solution
    made more efficient (no longer need dropWhile)

--- src/main/kotlin/kotlin4clojure/medium/palindromic_numbers/PalindromicNumbers.kt
+++ src/main/kotlin/kotlin4clojure/medium/palindromic_numbers/PalindromicNumbers.kt
@@ -1,23 +1,34 @@
 package kotlin4clojure.medium.palindromic_numbers
 
 fun palindromicNumbers(start: Long): Sequence<Long> {
-    var min = 1
-    var max = min * 10
-    var n = 0
-    var even = false
+    var max = Math.pow(start.toString().length.toDouble(), 10.0).toLong() * 10
+    var even = start.toString().isEven()
+    var n = startValue(start)
 
     return generateSequence({
         if (n == max) {
             if (even) {
-                min = n
                 max = n * 10
             } else {
-                n = min
+                n = max / 10
             }
             even = !even
         }
         val s = n.toString()
         n++
         (s + (if (even) s else s.dropLast(1)).reversed()).toLong()
-    }).dropWhile { it < start } // TODO remove dropWhile, set initial value of "n" to "start"
+    })
 }
+
+private fun startValue(start: Long) : Long {
+    val s = start.toString()
+    val endPoint = if (s.isEven()) s.length / 2 else s.length / 2 + 1
+    val n = s.substring(0, endPoint).toLong()
+    return if (n < s.reversed().substring(0, endPoint).toLong()) {
+        n + 1
+    } else {
+        n
+    }
+}
+
+fun String.isEven() = this.length % 2 == 0
