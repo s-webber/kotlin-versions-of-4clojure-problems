f915e103eda6d52e43e83ca0b4d111ed805cbeb7
s-webber <webbers.github@gmail.com>
2016-08-13 15:57:54 +0100

    transitive closure solution

--- src/main/kotlin/kotlin4clojure/hard/transitive_closure/TransitiveClosure.kt
+++ src/main/kotlin/kotlin4clojure/hard/transitive_closure/TransitiveClosure.kt
@@ -1,8 +1,15 @@
 package kotlin4clojure.hard.transitive_closure
 
+// TODO this is inefficient
 fun <T> transitiveClosure(input: Set<Pair<T, T>>): Set<Pair<T, T>> {
-    TODO("Add your solution here")
+    var current = input
+
+    do {
+        val size = current.size
+        current.forEach { p ->
             current.forEach {
+                if (it.second == p.first) {
+                    current += Pair(it.first, p.second)
                 }
             }
         }
