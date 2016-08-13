f8d83750dfedb19e791389a9f03524294fce97cb
s-webber <webbers.github@gmail.com>
2016-08-13 16:13:46 +0100

    prime numbers solution
    removed unnecessary semicolon

--- src/main/kotlin/kotlin4clojure/medium/prime_numbers/PrimeNumbers.kt
+++ src/main/kotlin/kotlin4clojure/medium/prime_numbers/PrimeNumbers.kt
@@ -3,7 +3,7 @@ package kotlin4clojure.medium.prime_numbers
 import java.math.BigInteger
 
 fun primeNumbers(n: Int): List<Int> {
-    val s = generateSequence(BigInteger("2"), { it.nextProbablePrime() });
+    val s = generateSequence(BigInteger("2"), { it.nextProbablePrime() })
     return s.take(n)
             .map { it.intValueExact() }
             .toList()
