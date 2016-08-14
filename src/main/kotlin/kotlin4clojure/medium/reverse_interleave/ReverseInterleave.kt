586246144f15d930f7d68e6e650d3ae204b45a30
s-webber <webbers.github@gmail.com>
2016-08-14 10:09:54 +0100

    reverse interleave solution

--- src/main/kotlin/kotlin4clojure/medium/reverse_interleave/ReverseInterleave.kt
+++ src/main/kotlin/kotlin4clojure/medium/reverse_interleave/ReverseInterleave.kt
@@ -1,5 +1,8 @@
 package kotlin4clojure.medium.reverse_interleave
 
-fun <T> reverseInterleave(input: List<T>, n: Int): List<List<T>> {
-    TODO("Add your solution here")
-}
+fun <T> reverseInterleave(input: List<T>, numberOfSubLists: Int) =
+    (0..numberOfSubLists - 1).map { n ->
+        (0..input.size - 1 step numberOfSubLists).map {
+            input[n + it]
+        }.toList()
+    }.toList()
