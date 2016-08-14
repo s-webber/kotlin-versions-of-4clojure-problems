892ebe7e46eba71af5ddb527bff2a47c28c21b9c
s-webber <webbers.github@gmail.com>
2016-08-14 09:47:46 +0100

    Pascal's trapezoid solution

--- src/main/kotlin/kotlin4clojure/easy/pascals_trapezoid/PascalsTrapezoid.kt
+++ src/main/kotlin/kotlin4clojure/easy/pascals_trapezoid/PascalsTrapezoid.kt
@@ -2,6 +2,13 @@ package kotlin4clojure.easy.pascals_trapezoid
 
 import java.math.BigInteger
 
-fun pascalsTrapezoid(input: List<BigInteger>): Sequence<List<BigInteger>> {
-    TODO("Add your solution here")
-}
+fun pascalsTrapezoid(input: List<BigInteger>) =
+    generateSequence(input, {previous ->
+        (0..previous.size).map {
+            when (it) {
+                0 -> previous.first()
+                previous.size -> previous.last()
+                else -> previous[it] + previous[it - 1]
+            }
+        }.toList()
+    })
