a8ff3cff685c3300fc8769b67de9230cb21f224a
s-webber <webbers.github@gmail.com>
2016-08-28 14:07:03 +0100

    the big divide solution

--- src/main/kotlin/kotlin4clojure/medium/the_big_divide/TheBigDivide.kt
+++ src/main/kotlin/kotlin4clojure/medium/the_big_divide/TheBigDivide.kt
@@ -2,6 +2,16 @@ package kotlin4clojure.medium.the_big_divide
 
 import java.math.BigInteger
 
+private val TWO = BigInteger("2")
+
 fun bigDivide(n: BigInteger, a: BigInteger, b: BigInteger): BigInteger {
-    TODO("Add your solution here")
+    val max = n - BigInteger.ONE
+    val c = a * b
+    val sumA = f(max, a)
+    val sumB = f(max, b)
+    val sumC = f(max, c)
+    return ((sumA + sumB) - sumC) / TWO
 }
+
+fun f(max: BigInteger, n: BigInteger) =
+    (max / n) * ((max - max.mod(n)) + n)
