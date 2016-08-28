050e82dce0a0ebd02ddda000fcd98131833174ec
s-webber <webbers.github@gmail.com>
2016-08-28 13:34:34 +0100

    least common multiple solution

--- src/main/kotlin/kotlin4clojure/easy/least_common_multiple/LeastCommonMultiple.kt
+++ src/main/kotlin/kotlin4clojure/easy/least_common_multiple/LeastCommonMultiple.kt
@@ -1,7 +1,25 @@
 package kotlin4clojure.easy.least_common_multiple
 
-fun leastCommonMultiple(vararg input: Ratio): Ratio {
-    TODO("Add your solution here")
+import kotlin4clojure.easy.greatest_common_divisor.greatestCommonDivisor
+
+fun leastCommonMultiple(vararg input: Ratio) =
+    input.reduce { a, b -> lcm(a, b) }
+
+fun lcm(a: Ratio, b: Ratio): Ratio {
+    val denominatorLcm = lcm(a.denominator, b.denominator)
+    val numeratorLcm = lcm(a.scale(denominatorLcm).numerator, b.scale(denominatorLcm).numerator)
+    val lcm = Ratio(numeratorLcm, denominatorLcm)
+    val gcd = greatestCommonDivisor(lcm.numerator, lcm.denominator)
+    return lcm / gcd
 }
+
+fun lcm(a: Int, b:Int): Int =
+    (a * b) / greatestCommonDivisor(a, b)
+
+operator fun Ratio.div(gcd: Int) =
+    Ratio(this.numerator / gcd, this.denominator / gcd)
+
+fun Ratio.scale(n: Int) =
+    Ratio(this.numerator * (n / this.denominator), this.denominator)
 
 data class Ratio(val numerator: Int, val denominator: Int = 1)
