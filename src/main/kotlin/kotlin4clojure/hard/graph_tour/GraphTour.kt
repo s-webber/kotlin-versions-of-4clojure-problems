dada3dcb062d03d1392fd3fb9d6ac65f1a9e02d7
s-webber <webbers.github@gmail.com>
2016-08-28 11:08:55 +0100

    graph tour solution

--- src/main/kotlin/kotlin4clojure/hard/graph_tour/GraphTour.kt
+++ src/main/kotlin/kotlin4clojure/hard/graph_tour/GraphTour.kt
@@ -1,5 +1,21 @@
 package kotlin4clojure.hard.graph_tour
 
+import kotlin4clojure.hard.graph_connectivity.graphConnectivity
+
 fun <T> graphTour(vararg input: Pair<T, T>): Boolean {
-    TODO("Add your solution here")
+    if (!graphConnectivity(input.toHashSet())) {
+        return false
+    }
+    return input.filter {
+        it.first != it.second
+    }.fold(mapOf<T, List<T>>(), { m, p  ->
+        m.append(p.first, p.second).append(p.second, p.first)
+    }).map {
+        it.value.size
+    }.count {
+        it % 2 == 1
+    } < 3
 }
+
+fun <T> Map<T, List<T>>.append(k: T, v: T): Map<T, List<T>> =
+    this + (k to (this[k]?.plus(v) ?: listOf(v)))
