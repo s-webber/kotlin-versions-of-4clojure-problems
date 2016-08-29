004c7dc878ceff6fd382654b224f93f828f9103e
s-webber <webbers.github@gmail.com>
2016-08-29 10:45:05 +0100

    win at tic-tac-toe solution

--- src/main/kotlin/kotlin4clojure/hard/win_at_tictactoe/WinAtTicTacToe.kt
+++ src/main/kotlin/kotlin4clojure/hard/win_at_tictactoe/WinAtTicTacToe.kt
@@ -1,7 +1,20 @@
 package kotlin4clojure.hard.win_at_tictactoe
 
 import kotlin4clojure.hard.analyze_a_tictactoe_board.Cell
+import kotlin4clojure.hard.analyze_a_tictactoe_board.lines
 
 fun winAtTicTacToe(player: Cell, board: Array<Array<Cell>>): Set<Pair<Int, Int>> {
-    TODO("Add your solution here")
+    return lines.map {
+        it.map { it to board[it.first][it.second] }
+    }.map {
+        it.groupBy { it.second }
+    }.map {
+        val numPlayersCells = it[player]?.size ?: 0
+        val emptyCell = it[Cell.E]?.first() ?: null
+        if (numPlayersCells == 2 && emptyCell != null) {
+            emptyCell.first
+        } else {
+            null
+        }
+    }.filterNotNull().toSet()
 }
