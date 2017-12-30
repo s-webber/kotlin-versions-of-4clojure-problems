8a77f8ebc7f38bf8082cf43ce1bd5dc07732275e
s-webber <webbers.github@gmail.com>
2017-12-30 13:51:58 +0000

    replaced unused variables with underscores

--- src/main/kotlin/kotlin4clojure/medium/rotate_list/RotateList.kt
+++ src/main/kotlin/kotlin4clojure/medium/rotate_list/RotateList.kt
@@ -2,7 +2,7 @@ package kotlin4clojure.medium.rotate_list
 
 fun <T> rotateList(n: Int, input: List<T>): List<T> {
     val step = n % input.size
-    return input.mapIndexed { i, t ->
+    return input.mapIndexed { i, _ ->
         var idx = i + step
         if (idx < 0) {
             idx += input.size
